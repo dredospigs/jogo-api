2677d944802913ac011cd5851f884b6813b4f18b
dredospigs <andrezinhogsoares@gmail.com>
2022-07-21 19:14:48 -0300

    fixes

--- src/app.js
+++ src/app.js
@@ -10,7 +10,12 @@ db.once("open", () => {
 
 const app = express()
 app.use(express.json())
-
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
 routes(app)
 
 module.exports = app 
